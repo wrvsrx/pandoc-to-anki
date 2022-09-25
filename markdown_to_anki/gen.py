45e3f0cf7491016ccc236b73d3a5cda0c42187e4
wrvsrx <wrvsrx@outlook.com>
2022-09-25 22:56:37 +0800

    feat: add css

--- markdown_to_anki/gen.py
+++ markdown_to_anki/gen.py
@@ -2,34 +2,46 @@ import json
 import sys
 import genanki
 
-model_id = 1245741901
+def main():
+    output_path = sys.argv[1]
 
-my_model = genanki.Model(
-    model_id=model_id,
-    name="Simple Model",
-    fields=[{"name": "Question"}, {"name": "Answer"}],
-    templates=[
-        {
-            "name": "Simple Card",
-            "qfmt": '<div class="card"><div class="question">{{Question}}</div></div>',
-            "afmt": '<div class="card">'
-            + '<div class="question">{{Question}}</div>'
-            + "<hr>"
-            + '<div class="answer">{{Answer}}</div>'
-            + "</div>",
-        },
-    ],
-)
+    model_id = 1245741901
 
-data = json.load(sys.stdin)
-deck = genanki.Deck(deck_id=int(data["deckId"]), name=data["deckTitle"])
-for note_data in data["notes"]:
-    note = genanki.Note(
-        model=my_model,
-        fields=[note_data["question"], note_data["answer"]],
-        guid=note_data["guid"],
+    my_model = genanki.Model(
+        model_id=model_id,
+        name="Simple Model",
+        fields=[{"name": "Question"}, {"name": "Answer"}],
+        templates=[
+            {
+                "name": "Simple Card",
+                "qfmt": '<div class="question">{{Question}}</div>',
+                "afmt": '<div class="question">{{Question}}</div>'
+                + "<hr>"
+                + '<div class="answer">{{Answer}}</div>',
+            },
+        ],
+        css="""
+        .card {
+            font-family: arial;
+            font-size: 20px;
+            color: black;
+            background-color: white;
+        }
+        """,
     )
-    print(note)
-    deck.add_note(note)
 
-genanki.Package(deck).write_to_file("test.apkg")
+    data = json.load(sys.stdin)
+    deck = genanki.Deck(deck_id=int(data["deckId"]), name=data["deckTitle"])
+    for note_data in data["notes"]:
+        note = genanki.Note(
+            model=my_model,
+            fields=[note_data["question"], note_data["answer"]],
+            guid=note_data["guid"],
+        )
+        print(note)
+        deck.add_note(note)
+
+    genanki.Package(deck).write_to_file(output_path)
+
+if __name__ == '__main__':
+    main()
