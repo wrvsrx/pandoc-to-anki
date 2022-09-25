d42730c749c5b5ceb630a5184914d014803a7779
wrvsrx <wrvsrx@outlook.com>
2022-09-25 23:04:56 +0800

    chore(nix): update flake

--- flake.nix
+++ flake.nix
@@ -20,31 +20,37 @@
           preferWheels = true;
         };
       in rec {
-        packages.default = stdenv.mkDerivation rec {
-          name = "markdown_to_anki";
-          src = ./.;
+        packages = rec {
+          markdown_to_anki-python = poetry2nix.mkPoetryApplication poetryAttrsSet;
+          markdown_to_anki = stdenv.mkDerivation rec {
+            name = "markdown_to_anki";
+            src = ./.;
+            buildInputs = [
+              (haskellPackages.ghcWithPackages (ps: with ps; [
+                pandoc
+                uuid
+                utf8-string
+                optparse-applicative
+                cryptohash-sha256
+                utf8-string
+              ]))
+            ];
+            buildPhase = ''
+              env --chdir=src ghc Main
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              install -m755 src/Main $out/bin/${name}
+            '';
+          };
+          default = markdown_to_anki;
+        };
+        devShell = mkShell {
+          inputsFrom = with packages; [ markdown_to_anki ];
           buildInputs = [
-            (haskellPackages.ghcWithPackages (ps: with ps; [
-              pandoc
-              uuid
-              utf8-string
-              optparse-applicative
-              cryptohash-sha256
-              utf8-string
-            ]))
             poetry
             (poetry2nix.mkPoetryEnv poetryAttrsSet)
           ];
-          buildPhase = ''
-            env --chdir=src ghc Main
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            install -m755 src/Main $out/bin/${name}
-          '';
-        };
-        devShell = mkShell {
-          inputsFrom = [ packages.default ];
         };
       }
   );
