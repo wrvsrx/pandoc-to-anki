ad0d82303b45a127032882c40e40940655202fdb
wrvsrx <wrvsrx@outlook.com>
2023-08-31 01:39:02 +0800

    chore: remove python code

--- flake.nix
+++ flake.nix
@@ -7,29 +7,13 @@
   };
   outputs = inputs': inputs'.flake-parts.lib.mkFlake { inputs = inputs'; } ({ inputs, ... }: {
     systems = [ "x86_64-linux" ];
-    perSystem = { system, pkgs, ... }:
-      let
-        poetryAttrsSet = {
-          projectDir = ./.;
-          overrides = pkgs.poetry2nix.overrides.withDefaults (final: prev: {
-            pandoc = prev.pandoc.overrideAttrs (old: {
-              nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.setuptools ];
-            });
-            genanki = prev.genanki.overrideAttrs (old: {
-              nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.pytest-runner ];
-            });
-          });
-          preferWheels = true;
-        };
-      in
-      {
-        packages = rec {
-          markdown-to-anki-python = inputs.poetry2nix.mkPoetryApplication poetryAttrsSet;
-          markdown-to-anki = pkgs.haskellPackages.callPackage ./default.nix { };
-          default = markdown-to-anki;
-        };
-        devShells.default = pkgs.callPackage ./shell.nix { };
+    perSystem = { system, pkgs, ... }: {
+      packages = rec {
+        markdown-to-anki = pkgs.haskellPackages.callPackage ./default.nix { };
+        default = markdown-to-anki;
       };
+      devShells.default = pkgs.callPackage ./shell.nix { };
+    };
   }
   );
 }
