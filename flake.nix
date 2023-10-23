a256e5bead074edadb681398ffa60d4c921a0a29
wrvsrx <wrvsrx@outlook.com>
2023-10-23 18:25:21 +0800

    chore(env): update flake.lock

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.follows = "flake-lock/nixpkgs";
     flake-parts.follows = "flake-lock/flake-parts";
   };
-  outputs = inputs': inputs'.flake-parts.lib.mkFlake { inputs = inputs'; } ({ inputs, ... }: {
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } ({ inputs, ... }: {
     systems = [ "x86_64-linux" ];
     perSystem = { system, pkgs, ... }: {
       packages = rec {
@@ -13,6 +13,7 @@
         default = markdown-to-anki;
       };
       devShells.default = pkgs.callPackage ./shell.nix { };
+      formatter = pkgs.nixpkgs-fmt;
     };
   }
   );
