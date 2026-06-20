110a413ffa25edd86e9c3588edb85f235df6b482
wrvsrx <wrvsrx@outlook.com>
2026-06-20 11:12:08 +0800

    chore(env): update flake.lock

--- flake.nix
+++ flake.nix
@@ -1,20 +1,23 @@
 {
   description = "pandoc ast to anki";
   inputs = {
-    flake-lock.url = "github:wrvsrx/flake-lock";
-    nixpkgs.follows = "flake-lock/nixpkgs";
-    flake-parts.follows = "flake-lock/flake-parts";
+    nur-wrvsrx.url = "github:wrvsrx/nur-packages";
+    nixpkgs.follows = "nur-wrvsrx/nixpkgs";
+    flake-parts.follows = "nur-wrvsrx/flake-parts";
   };
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } ({ inputs, ... }: {
-    systems = [ "x86_64-linux" ];
-    perSystem = { system, pkgs, ... }: {
-      packages = rec {
-        markdown-to-anki = pkgs.haskellPackages.callPackage ./default.nix { };
-        default = markdown-to-anki;
-      };
-      devShells.default = pkgs.callPackage ./shell.nix { };
-      formatter = pkgs.nixpkgs-fmt;
-    };
-  }
-  );
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { inputs, ... }: {
+        systems = [ "x86_64-linux" ];
+        perSystem = { system, pkgs, ... }: {
+          packages = rec {
+            markdown-to-anki = pkgs.haskellPackages.callPackage ./default.nix { };
+            default = markdown-to-anki;
+          };
+          devShells.default = pkgs.callPackage ./shell.nix { };
+          formatter = pkgs.nixpkgs-fmt;
+        };
+      }
+    );
 }
